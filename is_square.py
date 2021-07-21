e8dab75fa91e78ada87653aeed4bc71897caba66
Jackie890621 <jackie890621@gmail.com>
2021-07-21 14:50:16 +0800

    code

--- is_square.py
+++ is_square.py
@@ -1,27 +1,41 @@
-x1, y1 = input("Please input coordinate: ").split()
-x2, y2 = input("Please input coordinate: ").split()
-x3, y3 = input("Please input coordinate: ").split()
-x4, y4 = input("Please input coordinate: ").split()
+x = list()
+y = list()
+z = list()
+dist = list()
 
-x1 = int(x1)
-x2 = int(x2)
-x3 = int(x3)
-x4 = int(x4)
-y1 = int(y1)
-y2 = int(y2)
-y3 = int(y3)
-y4 = int(y4)
+f = open("test.txt", "r")
+case = f.readlines()
+f.close()
 
-a = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
-b = (x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1)
-c = (x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2)
-d = list()
-d.append(a)
-d.append(b)
-d.append(c)
-d.sort()
+for i in range(8):
+    temp = case[i].split(' ')
+    x.append(int(temp[0]))
+    y.append(int(temp[1]))
+    z.append(int(temp[2]))
 
-if (d[0] == d[1] and d[2] == d[0] + d[1]):
-    print("Square!!")
+for i in range(8):
+    for j in range(i + 1, 8):
+        temp = (x[j] - x[i]) * (x[j] - x[i]) + (y[j] - y[i]) * (y[j] - y[i]) + (z[j] - z[i]) * (z[j] - z[i])
+        dist.append(temp)
+dist.sort()
+
+flag = 1
+for i in range(1, len(dist)):
+    if (i <= 11):
+        if (dist[i] != dist[i - 1]):
+            flag = 0
+            break
+    elif (i <= 23):
+        if (dist[i] != 2 * dist[0]):
+            flag = 0
+            break
+    else:
+        if (dist[i] != 3 * dist[0]):
+            flag = 0
+            break
+
+if (flag):
+    print("It's a Square!!!")
 else:
-    print("Not a Square!!")
+    print("It's not a Square!!!")
+    
